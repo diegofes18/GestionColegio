2020d1d496f0f489cfa08bb14174dda0585d8ca2
diegofes18 <bermejodiego1@gmail.com>
2021-11-29 18:09:13 +0100

    visualizaciones finales faltan

--- src/practicaalg1/PracticaAlg1.java
+++ src/practicaalg1/PracticaAlg1.java
@@ -6,6 +6,7 @@ VICTOR CANELO
 package practicaalg1;
 
 import Asignatura.Asignatura;
+import Curso.Curs;
 import Estudiant.Estudiant;
 import Listas.ListaAsignaturas;
 import Listas.ListaCurso;
@@ -23,10 +24,14 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -38,6 +43,15 @@ public class PracticaAlg1 extends JFrame {
     ListaCurso listacurs;
     ListaEstudiante listaestudiant;
     ListaAsignaturas listaasig;
+    private JPanel panel;
+    private JComboBox combocurs;
+    private JLabel etiquetacurs;
+    private JTextArea mostrallista;
+    private BajaCurso bc;
+    private AltaCurso ac;
+    private MatricularEstudiante_1 me;
+    private BajaAsignaturaCurso bae;
+    private JScrollPane scrollPane;
 
     public static void main(String[] args) {
         try { //Control de l'aspecte
@@ -54,11 +68,25 @@ public class PracticaAlg1 extends JFrame {
     public PracticaAlg1() {
         setTitle("Gestion Colegio");
         listacurs = new ListaCurso();
+        combocurs = new JComboBox();
+        etiquetacurs = new JLabel("Seleccione un curso");
+        mostrallista = new JTextArea(16,58);
+        mostrallista.setVisible(false);
+        combocurs.setVisible(false);
+        etiquetacurs.setVisible(false);
+        panel=new JPanel();
         listaestudiant = new ListaEstudiante();
         listaasig = new ListaAsignaturas();
+        scrollPane = new JScrollPane(mostrallista,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVisible(false);
+        panel.add(scrollPane);
         this.setLayout(null);
         this.setResizable(false);
         this.setLocation(0, 0);
+        this.add(combocurs);
+        this.add(etiquetacurs);
+        
+        this.add(panel);
         setDefaultCloseOperation(PracticaAlg1.EXIT_ON_CLOSE);
         setSize(1600, 800);
         contenedor = getContentPane();
@@ -81,31 +109,35 @@ public class PracticaAlg1 extends JFrame {
             botones[i].setBackground(Color.WHITE);
             opciones.add(botones[i]);
         }
+        
+        combocurs.setBounds(250, 100, 250, 30);
+        etiquetacurs.setBounds(75, 100, 250, 30);
+        
+        panel.setBounds(1050,100,800,800);
         contenedor.add(opciones, BorderLayout.NORTH);
         JEditorPane viewer = new JEditorPane();
         viewer.setContentType("text/html");
         viewer.setEditable(false);
         viewer.setText(
-"<p>&nbsp;</p>\n" +
-"<h1 style=text-align: center; color: #3f7320;>PRACTICA 1: Gestion de Colegio</h1>\n" +
-"\n" +
-"\n" +
-"<h3 style=text-align: center; color: #3f7320;>by Diego Bermejo, Marc Ca&ntilde;ellas y Victor Canelo</p>\n" +
-"<h3 style=text-align: center; color: #3f7320;>😁</h3>\n" +
- "<p><br /><strong></strong></p>");
-        
-        JScrollPane scrollPane = new JScrollPane(viewer);
-        scrollPane.setBounds(50, 50, 300, 300);
-        contenedor.add(scrollPane, BorderLayout.CENTER);
-       
+                "<p>&nbsp;</p>\n"
+                + "<h1 style=text-align: center; color: #3f7320;>PRACTICA 1: Gestion de Colegio</h1>\n"
+                + "\n"
+                + "\n"
+                + "<h3 style=text-align: center; color: #3f7320;>by Diego Bermejo, Marc Ca&ntilde;ellas y Victor Canelo</p>\n"
+                + "<h3 style=text-align: center; color: #3f7320;>😁</h3>\n"
+                + "<p><br /><strong></strong></p>");
         //acciones
         botones[0].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                AltaCurso altac = new AltaCurso(listacurs,listaasig);
-                altac.setVisible(true);
-                listacurs = altac.getlistacurs();
-                listaasig=altac.getLista();
+                scrollPane.setVisible(false);
+                ac = new AltaCurso(listacurs, listaasig);
+                ac.setLocationRelativeTo(PracticaAlg1.this);
+                combocurs.setVisible(false);
+                etiquetacurs.setVisible(false);
+                ac.setVisible(true);
+                listacurs = ac.getlistacurs();
+                listaasig = ac.getLista();
                 listacurs.ordena();
                 listaasig.ordena();
             }
@@ -114,13 +146,17 @@ public class PracticaAlg1 extends JFrame {
         botones[1].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                MatricularEstudiante_1 me = new MatricularEstudiante_1(listaasig, listaasig.getlength());
+                me = new MatricularEstudiante_1(listaasig, listaasig.getlength());
+                scrollPane.setVisible(false);
+                me.setLocationRelativeTo(PracticaAlg1.this);
+                mostrallista.setVisible(false);
+                combocurs.setVisible(false);
+                etiquetacurs.setVisible(false);
                 me.setVisible(true);
                 Asignatura asignatura;
                 Estudiant estu;
-                if (listaestudiant.perteneceObj(me.getestu())) {
 
-                    System.out.print("ya existe");
+                if (listaestudiant.perteneceObj(me.getestu())) {
                     asignatura = listacurs.search(me.getStringasig());
                     estu = asignatura.getAsignatura_Estudiant().getEstudiant(me.getestu().getDNI());
                     estu.addasig(asignatura);
@@ -130,23 +166,25 @@ public class PracticaAlg1 extends JFrame {
                     estu.getEstudiant_Asignatura().Mostrar_Lista();
 
                 } else {
-                    //crear estudiante y añadir a la lista
-                    estu = me.getestu();
-                    estu.setListaasig(new Estudiant_Asignatura());
-                    listaestudiant.addestu(estu);
-                    //coger asignatura de lista curso
-                    asignatura = listacurs.search(me.getStringasig());
-                    //añadir estudiante a asignatura
-                    asignatura.addestu(estu);
-                    //añadir asignatura a lista asignatura del estudiante
-                    estu.addasig(asignatura);
-                    //actualizar lista curso con la asignatura cambiada
-                    listacurs.actualiza(asignatura, estu);
-                    listacurs.ordena();
-                    listaestudiant.ordena();
-                    System.out.println(me.getStringasig());
-                    System.out.println(me.getEstu());
-                    System.out.print(me.getestu().getEstudiant_Asignatura().Mostrar_Lista());
+                    if (listaasig.getlength() != 0) {
+                        estu = me.getestu();
+                        estu.setListaasig(new Estudiant_Asignatura());
+                        listaestudiant.addestu(estu);
+                        //coger asignatura de lista curso
+                        asignatura = listacurs.search(me.getStringasig());
+                        //añadir estudiante a asignatura
+                        asignatura.addestu(estu);
+                        //añadir asignatura a lista asignatura del estudiante
+                        estu.addasig(asignatura);
+                        //actualizar lista curso con la asignatura cambiada
+                        listacurs.actualiza(asignatura, estu);
+                        listacurs.ordena();
+                        listaestudiant.ordena();
+                        System.out.println(me.getStringasig());
+                        System.out.println(me.getEstu());
+                        System.out.print(me.getestu().getEstudiant_Asignatura().Mostrar_Lista());
+                    }
+
                 }
             }
 
@@ -154,25 +192,35 @@ public class PracticaAlg1 extends JFrame {
         botones[2].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                BajaCurso bc=new BajaCurso(listacurs,listacurs.getlength());
-                if(listacurs.getlength()==0){
+                scrollPane.setVisible(false);
+                bc = new BajaCurso(listacurs, listacurs.getlength());
+                bc.setLocationRelativeTo(PracticaAlg1.this);
+                mostrallista.setVisible(false);
+                combocurs.setVisible(false);
+                etiquetacurs.setVisible(false);
+                if (listacurs.getlength() == 0) {
                     bc.setVisible(false);
                     bc.MostrarMensaje();
-                }else{
-                bc.setVisible(true);
-                }   
+                } else {
+                    bc.setVisible(true);
+                }
             }
         });
 
         botones[3].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                BajaAsignaturaCurso bc=new BajaAsignaturaCurso(listacurs,listaasig,listaestudiant);
-                if(listaasig.getlength()==0){
-                    bc.setVisible(false);
-                    bc.MostrarMensaje();
-                }else{
-                bc.setVisible(true);
+                scrollPane.setVisible(false);
+                bae = new BajaAsignaturaCurso(listacurs, listaasig, listaestudiant);
+                bae.setLocationRelativeTo(PracticaAlg1.this);
+                mostrallista.setVisible(false);
+                combocurs.setVisible(false);
+                etiquetacurs.setVisible(false);
+                if (listaasig.getlength() == 0) {
+                    bae.setVisible(false);
+                    bae.MostrarMensaje();
+                } else {
+                    bae.setVisible(true);
                 }
             }
         });
@@ -180,7 +228,23 @@ public class PracticaAlg1 extends JFrame {
         botones[4].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                  viewer.setText(listaasig.toString());
+                if (listacurs.getlength() == 0) {
+                    JOptionPane.showMessageDialog(null, "No hay ningun curso dado de alta");
+                } else {
+                    combocurs.setVisible(true);
+                    scrollPane.setVisible(true);
+                    //rellenar combo
+                    String name;
+                    for (int i = 0; i < listacurs.getlength(); i++) {
+                        name = listacurs.returnCurs(i).getNom();
+                        combocurs.addItem(name);
+                    }
+                    etiquetacurs.setVisible(true);
+                    Curs c = listacurs.returnCurs(combocurs.getSelectedIndex());
+                    mostrallista.setVisible(true);
+                    mostrallista.setText(c.getlista().toString());
+
+                }
             }
         });
 
@@ -195,7 +259,7 @@ public class PracticaAlg1 extends JFrame {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 viewer.setText(listacurs.toString());
-            
+
             }
         });
 
